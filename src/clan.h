19fdbe8f8854cbf52f4e3784761d9c4cc425f51b
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2026-01-01 05:27:53 -0500

    Add clan PvP type system

--- src/clan.h
+++ src/clan.h
@@ -5,11 +5,17 @@
 #define CLAN_NAME_LEN 64
 #define CLAN_DISPLAY_LEN 64
 
+/* Clan PvP types */
+#define CLAN_PVP_NO_PVP    0
+#define CLAN_PVP_ALWAYS    1
+#define CLAN_PVP_TOGGLE    2
+
 struct clan_data {
   int id;
   long leader_idnum;
   char name[CLAN_NAME_LEN];
   char display_name[CLAN_DISPLAY_LEN];
+  int pvp_type;
   struct clan_data *next;
 };
 
@@ -19,12 +25,15 @@ void clan_shutdown(void);
 int clan_exists(int clan_id);
 const char *clan_name_by_id(int clan_id);
 const char *clan_display_name_by_id(int clan_id);
+const char *clan_pvp_type_name(int pvp_type);
+int clan_pvp_type_by_id(int clan_id);
 int clan_id_by_name(const char *name);
 int clan_next_id(void);
 
 int clan_create_and_save(int new_id, long leader_idnum, const char *name);
 int clan_set_name_and_save(int clan_id, const char *name);
 int clan_set_display_name_and_save(int clan_id, const char *display_name);
+int clan_set_pvp_type_and_save(int clan_id, int pvp_type);
 int clan_save_all(void);
 
 #endif
