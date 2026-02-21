724adc9392997da83c5f9d95a0dfeeb6e337ec60
Tony <112714443+TonyDeRosia@users.noreply.github.com>
2026-02-20 21:05:27 -0500

    Fix AI test mob prototype loading at boot

--- src/db.c
+++ src/db.c
@@ -477,6 +477,7 @@ void boot_world(void)
 
   log("Loading mobs and generating index.");
   index_boot(DB_BOOT_MOB);
+  log("AI test mob 19500 real_mobile=%d", real_mobile(19500));
 
   log("Loading objs and generating index.");
   index_boot(DB_BOOT_OBJ);
